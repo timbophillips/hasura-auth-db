a18d11b3e836b774710a0fed6a8a3afbc8abe787
timbophillips <timothy.john.phillips@gmail.com>
2022-12-27 20:42:27 +0800

    slick

--- hasura/migrations/default/1646746132261_seed/up.sql
+++ hasura/migrations/default/1646746132261_seed/up.sql
@@ -12,17 +12,6 @@ CREATE TABLE public.users (
     "group" text DEFAULT '' :: text
 );
 
-CREATE SEQUENCE public.users_id_seq AS integer START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
-
-ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
-
-ALTER TABLE
-    ONLY public.users
-ALTER COLUMN
-    id
-SET
-    DEFAULT nextval('public.users_id_seq' :: regclass);
-
 ALTER TABLE
     ONLY public.users
 ADD
